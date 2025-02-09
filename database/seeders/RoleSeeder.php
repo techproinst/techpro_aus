9aba48786d8b80e975a94ccfa6741b63962f0fce
swiftly01 <kareemkazeem100@gmail.com>
2025-02-09 19:56:55 +0100

    Refactored role and permissions

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
@@ -15,35 +16,38 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        $admin = Role::create(['name' => 'super-admin']);
-        Permission::insert([
-            ['name' => 'create role', 'guard_name' => 'web'],
-            ['name' => 'view role',   'guard_name' => 'web'],
-            ['name' => 'update role',   'guard_name' => 'web'],
-            ['name' => 'delete role',   'guard_name' => 'web'],
-            ['name' => 'view permission',   'guard_name' => 'web'],
-            ['name' => 'create permission',   'guard_name' => 'web'],
-            ['name' => 'update permission',   'guard_name' => 'web'],
-            ['name' => 'delete permission',   'guard_name' => 'web'],
-        ]);
+        $admin = Role::firstOrCreate(['name' => 'super-admin']);
 
-        $admin->givePermissionTo([
-            'create role',
-             'view role',
-             'update role',
-             'delete role',
-             'view permission',
-             'create permission',
-             'update permission',
-             'delete permission',
-            
+        $permissions = [
+            'create role', 'view role', 'update role', 'delete role',
+            'view permission', 'create permission', 'update permission', 'delete permission',
+            'create user', 'view user', 'update user', 'delete user',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission,
+                'guard_name' => 'web',
+
             ]);
+        }
 
+        if($admin){
+
+            $admin->givePermissionTo($permissions);
+        }
+
+
+        
          $user = User::find(1);
 
          if ($user) {
 
             $user->assignRole('super-admin');
+            
+        } else {
+
+            Log::warning('User with ID 1 not found. Super-admin role not assigned.');
         }
         
     }
