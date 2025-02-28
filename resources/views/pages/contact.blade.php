288fc39c12fd4f3d9074645f7fd76ccaaab82b6d
techproinst <techproinstitutenigeria@gmail.com>
2025-02-28 17:48:08 +0100

    init

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,27 @@
+<x-pages-layout>
+  <x-slot:title>
+    Business Analysis Course
+  </x-slot:title>
+
+  <x-slot:link>
+    Contact Us
+  </x-slot:link>
+
+  <x-slot:course_title>
+    1 on 1 Session
+  </x-slot:course_title>
+
+  
+
+   <div class="container-fluid ">
+    <div class="row">
+      <div class="col text-center mt-1 mt-md-3  mb-3">
+        <h1 class="title">Book a Session</h1>
+        <p>Schedule a personal session</p>
+      </div>
+    </div>
+
+ 
+
+
+</x-pages-layout>
